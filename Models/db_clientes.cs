5f0a40082338a40c5e0d8da5f192d50095f5999b
robingiraldo123 <robinsorg@si.edu.co>
2021-09-21 21:23:36 -0500

    Añadiendo base de datos

---
+++ Models/db_clientes.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collection.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Connection {
+  public class BdClientes {
+      [key]
+      public int id { get; set; }
+
+      public string name { get; set; }
+  }
+}
---
+++ Models/db_clientesContext.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collection.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Connection {
+  public class BdClientesContext {
+
+  }
+}
